6971346a2a5d9324961b1916c1bb5726c0578ae1
Solomon Mengesha Kebede <113286991+SolomonM-Kebede@users.noreply.github.com>
2025-11-13 19:14:09 +0100

    and more

--- main.cpp
+++ main.cpp
@@ -9,18 +9,11 @@
  * compiler optimization level use -O1, -O2 or -O3
  * to change compiling mood from debug to release :
  * clang++ -std=c++17 -O3 -DNDEBUG -o game_release main.cpp WorldClass.cpp Interface.cpp
- * for ex.1.4 and 1.5 use the predefined autorun methods by commented out
+ *
  * @return
  */
 int main() {
-
+    //build your own Grid
     Interface interface;
-    interface.CLI(); //under normal circumstances
-    //interface.autoRun("p67_snark_loop.txt", 2000); // run after disabling print and stability
-
-    // std::vector<std::pair<int, int>> sizes= {
-    //     {10,10}, {20,20}, {100, 100}, {1000, 1000}, {100000, 100000}
-    // };
-    // for (auto[h,w] : sizes)
-    //     interface.autoRunWithSize("p67_snark_loop.txt", h, w, 100);
+    interface.CLI();
 }
